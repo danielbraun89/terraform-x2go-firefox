5587466eb3dad8adddf452f96a87db1b1505baa0
Daniel Braun <daniel.braun@outlook.co.il>
2020-09-20 17:31:56 +0300

    added better sequrity group for ssh and connections removed python installation

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 REPO_NAME="terraform-x2go-firefox"
-REPO_URL="https://github.com/AleksanderGrzybowski/${REPO_NAME}.git"
+REPO_URL="https://github.com/danielbraun89/${REPO_NAME}.git"
 UBUNTU_PASSWORD="x2goletmein2020"
 
 git clone ${REPO_URL}
